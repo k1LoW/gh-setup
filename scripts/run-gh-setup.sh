a3af5ec561d2b8ba189def6df8588551aa02ca4d
k1LoW <k1lowxb@gmail.com>
2023-02-12 22:35:48 +0900

    Add action.yml

---
+++ scripts/run-gh-setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+bin=${GH_SETUP_BIN}
+token=${GH_SETUP_GITHUB_TOKEN}
+repo=${GH_SETUP_REPO}
+bindir=${GH_SETUP_BIN_DIR}
+force=${GH_SETUP_FORCE}
+
+${bin} --repo ${repo} --bin-dir=${GH_SETUP_BIN_DIR}
