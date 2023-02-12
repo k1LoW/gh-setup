52603fea03cedc88314e675d2d12ef9b04eaa8c7
k1LoW <k1lowxb@gmail.com>
2023-02-12 23:22:53 +0900

    Add option `--version` `--os` `--arch`

--- cmd/root.go
+++ cmd/root.go
@@ -38,6 +38,8 @@ var (
 	force     bool
 )
 
+var opt = &gh.AssetOption{}
+
 var rootCmd = &cobra.Command{
 	Use:     "gh-setup",
 	Short:   "Setup asset of Github Releases",
@@ -80,4 +82,7 @@ func init() {
 	rootCmd.Flags().StringVarP(&ownerrepo, "repo", "R", "", "repository using the OWNER/REPO format")
 	rootCmd.Flags().StringVarP(&binDir, "bin-dir", "", "", "bin directory for setup")
 	rootCmd.Flags().BoolVarP(&force, "force", "f", false, "enable force setup")
+	rootCmd.Flags().StringVarP(&opt.Version, "version", "", "", "release version")
+	rootCmd.Flags().StringVarP(&opt.OS, "os", "", "", "specify OS of asset")
+	rootCmd.Flags().StringVarP(&opt.Arch, "arch", "", "", "specify arch of asset")
 }
