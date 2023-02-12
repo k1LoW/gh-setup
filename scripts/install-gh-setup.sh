7597c5982fbc40c3a972b56fd0ff67690e0d96df
k1LoW <k1lowxb@gmail.com>
2023-02-12 22:41:08 +0900

    Add action test

--- scripts/install-gh-setup.sh
+++ scripts/install-gh-setup.sh
@@ -2,30 +2,34 @@
 set -e
 
 token=${GH_SETUP_GITHUB_TOKEN}
+if [ -z "${token}" ]; then
+  token=${GITHUB_TOKEN}
+fi
 repo="k1LoW/gh-setup"
-tag="$(curl -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${token}" https://api.github.com/repos/${repo}/releases/latest | grep tag_name | awk -F':' '{print $2}' | awk -F'\"' '{print $2}')"
+tag="$(curl -sL -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${token}" https://api.github.com/repos/${repo}/releases/latest | grep tag_name | awk -F':' '{print $2}' | awk -F'\"' '{print $2}')"
 arch="$(uname -m)"
 
 if uname -a | grep Msys > /dev/null; then
   if [ $arch = "x86_64" ]; then
     exe="gh-setup_${tag}_windows_amd64.exe"
   fi
-  bin=${TEMP}/gh-setup
+  bin="${TEMP}/gh-setup"
 elif uname -a | grep Darwin > /dev/null; then
   if [ $arch = "x86_64" ]; then
     exe="gh-setup_${tag}_darwin_amd64"
   elif [ $arch = "arm64" ]; then
     exe="gh-setup_${tag}_darwin_arm64"
   fi
-  bin=${TMPDIR}gh-setup
+  bin="${TMPDIR}gh-setup"
 elif uname -a | grep Linux > /dev/null; then
   if [ $arch = "x86_64" ]; then
     exe="gh-setup_${tag}_linux_amd64"
   fi
-  bin=/tmp/gh-setup
+  bin="/tmp/gh-setup"
 fi
 
 # download
 curl -sL -o ${bin} https://github.com/k1LoW/gh-setup/releases/download/${tag}/${exe}
 chmod +x ${bin}
+${bin} -v
 echo "bin=${bin}" >> ${GITHUB_OUTPUT}
--- scripts/run-gh-setup.sh
+++ scripts/run-gh-setup.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-bin=${GH_SETUP_BIN}
+bin="${GH_SETUP_BIN}"
 token=${GH_SETUP_GITHUB_TOKEN}
+if [ -z "${token}" ]; then
+  token=${GITHUB_TOKEN}
+fi
 repo=${GH_SETUP_REPO}
 bindir=${GH_SETUP_BIN_DIR}
 force=${GH_SETUP_FORCE}
